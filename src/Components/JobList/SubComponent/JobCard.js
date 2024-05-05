675627eebfdafa8d68cad59d5b777efdad1088f8
Mohit Soni <71427830+way2mohits@users.noreply.github.com>
2024-05-05 21:35:12 +0530

    job card styling done

--- src/Components/JobList/SubComponent/JobCard.js
+++ src/Components/JobList/SubComponent/JobCard.js
@@ -26,11 +26,18 @@ export const JobCard = ({jobObj})=>{
                 <div className="jobLocation">{jobObj.location}</div>
             </div>
         </div>
-        {/* <div className="salaryDetails">
-            Estimated salary range : {jobObj.minJdSalary ?? 0}LPA - {jobObj.maxJdSalary ?? 0}LPA
-        </div>
+        <p className="salaryDetails">
+            Estimated salary range : {jobObj.minJdSalary ?? 0}LPA - {jobObj.maxJdSalary ?? 0}LPA &#x2705;
+        </p>
         <div className="aboutCompany">
-
-        </div> */}
+            <p className="aboutCompanyText">About Company:</p>
+            <p style={{"padding": "0","marginTop": "0","fontWeight": "700"}}>About us</p>
+            <p className="jobDescription">{jobObj.jobDetailsFromCompany}</p>
+            <div className="viewJobLink"><a href={jobObj.jdLink}>View Job</a></div>
+        </div>
+        <div className="minimumExperienceText">Minimum Experience</div>
+        <div>{jobObj.minExp} years</div>
+        <div className="easyApplyButton">&#x26A1;Easy Apply</div>
+        <div className="referalButton">Unlock Referal asks</div>
     </div>);
 }
