976e46c94656fb566a4cedd155f4b848195dad28
Mohit Soni <71427830+way2mohits@users.noreply.github.com>
2024-05-06 15:32:41 +0530

    loader added

--- src/Components/JobList/JobList.jsx
+++ src/Components/JobList/JobList.jsx
@@ -5,7 +5,8 @@ import { addJobData } from "../../Redux/slice/jobListSlice";
 import { fetchJobData, getFilteredJobData } from "./JobListUtils";
 
 export const JobList = () => {
-  const [offSet, setOffSet] = useState();
+  const [isLoading,setIsLoading] = useState(false);
+  const [offSet, setOffSet] = useState(0);
   const jobListData = useSelector((state) => state.jobList);
   const filters = useSelector(state=>state.filters);
   const filteredJobList = getFilteredJobData(jobListData,filters);
@@ -13,9 +14,11 @@ export const JobList = () => {
   const dispatch = useDispatch();
   
   useEffect(()=>{
+    setIsLoading(true);
     fetchJobData(offSet).then((result)=>{
         dispatch(addJobData(result.jdList));
         setOffSet((prevState)=>prevState+10);
+        setIsLoading(false);
     });
   },[])
   const handleScroll = async () => {
@@ -24,10 +27,13 @@ export const JobList = () => {
       clientHeight + document.documentElement.scrollTop + 1 >=
       scrollHeight
     ) {
-      fetchJobData(offSet).then((result)=>{
-        dispatch(addJobData(result.jdList));
-        setOffSet((prevState)=>prevState+10);
-    });
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("wheel", handleScroll);
+      setIsLoading(true);
+      const result = await fetchJobData(offSet);
+      dispatch(addJobData(result.jdList));
+      setOffSet((prevState)=>prevState+10);
+      setIsLoading(false);
     }
   };
   useEffect(() => {
@@ -35,10 +41,11 @@ export const JobList = () => {
     window.addEventListener("wheel", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
-      window.addEventListener("wheel", handleScroll);
+      window.removeEventListener("wheel", handleScroll);
     };
   });
   return <div style={{display:"flex",flexWrap:"wrap"}}>
   {filteredJobList.map((jobObj,index)=><JobCard key={jobObj.jdUid+index} jobObj={jobObj} />)}
+  {isLoading && <div style={{"fontSize": "3em"}}>Please wait loading more jobs....</div>}
   </div>
 };
